772415c631f5a9ebbcd4055a6079589b2c7c94f0
EddisonChen <eddisonchen1@gmail.com>
2023-12-14 13:36:37 -0500

    changed from sql date to java.util.date

--- 631-backend/dbmgtproject/src/main/java/com/example/dbmgtproject/controller/InPatientManagementController.java
+++ 631-backend/dbmgtproject/src/main/java/com/example/dbmgtproject/controller/InPatientManagementController.java
@@ -11,7 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.Date;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
